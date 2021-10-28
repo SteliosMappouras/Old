1b6a27b66b46f6915df170f2372a70e8438542df
SteliosMappouras <73937060+SteliosMappouras@users.noreply.github.com>
2021-10-28 18:48:24 +0300

    first steps - cleaning
    first steps - cleaning

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import datetime as dt
 
 
-st.title('Uber pickups in NYC')
-st.title('Uber pickups in NYC')
-st.title('Uber pickups in NYC')
-st.title('Uber pickups in NYC')
-st.title('Uber pickups in NYC')
-st.title('Uber pickups in NYC')
+st.title('Data Science Project:')
+st.caption('Stelios Mappouras\nIoannis Volonakis\nSavvina Rousou\nMarios Kyriakides')
+
+
+store = pd.read_csv('store.csv')
+
+st.dataframe(store.head())
