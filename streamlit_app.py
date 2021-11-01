bc856c393ec1558f8d822266d543e0d642b111a5
SteliosMappouras <73937060+SteliosMappouras@users.noreply.github.com>
2021-11-01 23:56:18 +0200

    tst
    tst

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,48 @@ import seaborn as sns
 import datetime as dt
 
 
-st.title('Data Science Project:')
-st.caption('Stelios Mappouras\nIoannis Volonakis\nSavvina Rousou\nMarios Kyriakides')
+#st.title('Data Science Project:')
+#st.caption('Stelios Mappouras\nIoannis Volonakis\nSavvina Rousou\nMarios Kyriakides')
 
 
 store = pd.read_csv('store.csv')
 
-st.dataframe(store.head())
+#get row, column count
+size = store.shape
+
+#seperate numeric with categorical columns
+#get numeric
+store_numeric = store.select_dtypes(include=[np.number])
+numeric_cols = store_numeric.columns.values
+
+#get categorical
+store_categorical = store.select_dtypes(exclude=[np.number])
+categorical_cols = store_categorical.columns.values
+
+print("dataset size:", size)
+print("numeric ", numeric_cols)
+print("categorical: ", categorical_cols)
+
+
+#calculate and plot percentage of values missing from each column then store the info in a dataframe
+values_list = list()
+cols_list=list()
+
+for col in store.columns:
+    missing=np.mean(store[col].isnull())*100
+    cols_list.append(col)
+    values_list.append(missing)
+missing_df = pd.DataFrame()
+missing_df['col'] = cols_list
+missing_df['missing'] = values_list
+
+missing_df.loc[missing_df.missing > 0].plot(kind='bar', figsize=(12,8))
+
+info = store.info()
+print(info)
+
+
+#plt.show()
+
+
+print(store.isnull().sum())
